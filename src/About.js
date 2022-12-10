9040876400e18d4dfd3215f1525e67dad85e7f54
Leonardo Schmitt <66500082+LeonardoSchmittK@users.noreply.github.com>
2022-12-09 22:37:21 -0300

    commit

---
+++ src/About.js
@@ -0,0 +1,16 @@
+const About = () => {
+    return <>
+        <h1>Leonardo Schmitt Kreuch</h1>
+        <p>Text</p>
+        <h2>Habilidades</h2>
+        <ul>
+            <li>Js</li>
+            <li>Css</li>
+            <li>HTML</li>
+        </ul>
+    </>
+       
+}
+
+
+export default About;
